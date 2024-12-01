f74d3fdfe99eb9ced55617ca07c7521a38d88231
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-12-01 06:20:31 -0500

    understanding the String 'drop' when scope is no longer valid

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     }                       // this scope is now over, and _s is no longer valid
 
     // use of String as an example to study ownership
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s);
+    {
+        let mut s = String::from("hello"); // s is valid from this point forward
+        s.push_str(", world!");                // do stuff with s
+        println!("{}", s);
+    }                                                // the scope is now over, and s is no
+                                                     // longer valid
 }
